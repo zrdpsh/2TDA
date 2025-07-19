b60c08ada3de9565ab09f04264c04f9cfc102831
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-07-20 00:00:03 +0300

    Create Printable.java

---
+++ Printable.java
@@ -0,0 +1,33 @@
+// 3. Cтруктурное наследование - добавляем некоторое качественно новое свойство, семантически не привязанное к родительскому классу:
+interface Printable {
+  void print();
+}
+
+// Когда наследовать имеет смысл именно абстрактное свойство - здесь обоим классам нужно *что-то* печатать,
+// но это *что-то* в каждом случае принципиально своё
+
+class TextDocument implements Printable {
+  private String documentContent;
+
+  public TextDocument(String contents) {
+    this.documentContent = contents;
+  }
+
+  @Override
+  public void print() {
+    System.out.println("Document contents are: " + documentContent);
+  }
+}
+
+class Image implements Printable {
+  private String fileName;
+
+  public Image(String fileName) {
+    this.fileName = fileName;
+  }
+
+  @Override
+  public void print() {
+    System.out.println("This is the image named " + fileName);
+  }
+}
